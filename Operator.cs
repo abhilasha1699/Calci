af3de241e8b3a54ea7e86cf479209ccd58d127db
abhilasha1699 <77963939+abhilasha1699@users.noreply.github.com>
2021-02-01 12:16:04 +0530

    Add files via upload

---
+++ Operator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calci
+{
+    public static class Operator
+    {
+        public static double Calculate(double first, double second, string Operator)
+        {
+            double result = 0;
+            switch(Operator)
+            {
+                case "+":
+                    result = first + second;
+                    break;
+
+                case "-":
+                    result = first - second;
+                    break;
+
+                case "×":
+                    result = first * second;
+                    break;
+
+                case "÷":
+                    result = first / second;
+                    break;
+
+                case "%":
+                    result = first / 100;
+                    break;
+            }
+            return result;
+        }
+    }
+}
